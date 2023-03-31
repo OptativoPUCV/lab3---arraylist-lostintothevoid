64bb03ba8217727d6f338181ab14fde52ecea6e7
Replit user <>
2023-03-31 22:06:29 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -106,8 +106,5 @@ int get_size(ArrayList * l){
 
 //remove elements
 void clean(ArrayList * l){
-  ArrayList *aux = createList();
-  l->capacity=aux->capacity;
-  l->data=aux->data;
-  l->size=aux->size;
+  ArrayList *l= createList();
 }
