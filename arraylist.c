40997478d1f3c17a24abc496c0027510b489df9f
Replit user <>
2023-03-31 12:35:32 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -35,16 +35,16 @@ void append(ArrayList * l, void * data){
 void push(ArrayList * l, void * data, int i){
   if(i>l->size) return;
   if(l->size == l->capacity){
-    l->data=realloc(l->data, sizeof(void)*l->capacity*2);
+    l->data=realloc(l->data, sizeof(void*)*l->capacity*2);
     l->capacity = l->capacity*2;
       
   }
   else{
-    for(int j=l->size; j>=i; j--){
-      l->data[j+1]=l->data[j];
+    for(int j=l->size; j>i; j--){
+      l->data[j]=l->data[j-1];
     }
     l->data[i]=data;
-    //l->size++;
+    l->size++;
   }
 
 }
